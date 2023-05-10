88df76a2b054e6fbfbf6e321a67a01603200e6c7
celiacapereira <ccap98@gmail.com>
2023-05-10 17:25:28 +0100

    ui?

--- create_file.py
+++ create_file.py
@@ -6,7 +6,7 @@ from snowflake.connector.pandas_tools import write_pandas
 
 print('estou a ler')
 
-# print(os.environ["SF_USERNAME"])
+print(os.environ["SF_ACCOUNT"])
 
 conn = snowflake.connector.connect(
     user=os.environ["SF_USERNAME"],
