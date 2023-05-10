a5c4e9f3090e1e322235793d3dfa4737eee4b4b6
celiacapereira <ccap98@gmail.com>
2023-05-10 18:03:09 +0100

    test1

--- create_file.py
+++ create_file.py
@@ -11,8 +11,8 @@ conn = snowflake.connector.connect(
     password=os.environ["SF_PASSWORD"],
     # account = 'px51283.eu-west-3.aws'
     account=os.environ["SF_ACCOUNT"],
-    database = 'DEV',
-    schema = 'RAW'    
+    database = 'DEV'#,
+#    schema = 'RAW'    
     )
 
 cursor = conn.cursor()
