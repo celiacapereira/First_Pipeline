7c4520a6074b745e0ea75d0202fbc8b54671b871
celiacapereira <ccap98@gmail.com>
2023-05-10 14:22:51 +0100

    connection to snowflake

--- create_file.py
+++ create_file.py
@@ -1,2 +1,15 @@
+import snowflake.connector
+import os
+from snowflake.connector import *
+import pandas as pd
+from snowflake.connector.pandas_tools import write_pandas
 
-print('Hello world!')
+conn = snowflake.connector.connect(
+    user=os.environ['SF_USERNAME'],
+    password=os.environ['SF_PASSWORD'],
+    account=os.environ['SF_ACCOUNT']
+    )
+# print(conn.user)
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM DEV.RAW.TITANIC_TRAIN_RAW;")
+print("connection is donne")
