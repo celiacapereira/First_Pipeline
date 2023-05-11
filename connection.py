311aaae6752011f10939d8e57aa53f4437ee7225
celiacapereira <ccap98@gmail.com>
2023-05-11 10:37:15 +0100

    test variables name

--- connection.py
+++ connection.py
@@ -1,10 +1,10 @@
 import os
 
-SF_ACCOUNT = os.environ['SF_ACCOUNT']
-SF_PASSWORD = os.environ['SF_PASSWORD']
-SF_USERNAME = os.environ['SF_USERNAME']
+account = os.environ['SF_ACCOUNT']
+password = os.environ['SF_PASSWORD']
+username= os.environ['SF_USERNAME']
 
 
-print(f"SF ACCOUNT IS:  {SF_ACCOUNT}")
-print(f"SF PASSWORD IS:  {SF_PASSWORD}")
-print(f"SF USERNAME IS:  {SF_USERNAME}")
+print(f"SF ACCOUNT IS:  {account}")
+print(f"SF PASSWORD IS:  {password}")
+print(f"SF USERNAME IS:  {username}")
